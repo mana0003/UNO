8e6d52440005a7db2854884caba0d473354979a4
Manal Amjad <manalamjad2003@gmail.com>
2024-11-15 08:12:42 +0100

    TUI UNO still not done

--- uno/src/main/scala/Hand2.scala
+++
@@ -1,5 +0,0 @@
-/*case class Hand2(cards: List[Card] = randomCards(7)) {
-  def addCard(card: Card): Hand = copy(cards = card :: cards)
-
-  def removeCard(card: Card): Hand = copy(cards = cards diff List(card))
-}*/
--- uno/src/main/scala/Round2.scala
+++
@@ -1,5 +0,0 @@
-/*case class Round2(
-                  val players: List[Player] = (0 until 2).map(i => Player(i, Hand())).toList,
-                  val topCard: Card = randomCard,
-                  val currentPlayer: Int = 0
-                ) {}*/
