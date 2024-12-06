e6d8a394e0404d81d16e7e5189b38cb7919ef375
Manal Amjad <manalamjad2003@gmail.com>
2024-12-06 12:12:53 +0100

    undo, redo

--- src/main/scala/view/TUI.scala
+++ src/main/scala/view/TUI.scala
@@ -5,37 +5,64 @@ import controller.*
 import util.*
 import scala.io.StdIn
 import scala.io.AnsiColor._
-//import util.{Event, Observer}
 import util.Event.{Quit, Start}
+import scala.util.{Try, Success, Failure}
 
 class TUI(val controller: UnoController) extends Observer {
   controller.add(this)
+  private val commandManager = new CommandManager
 
   private def processInputLine(input: Int, handSize: Int): Unit = {
-    if (input < 1 || input > handSize) {
-      println("Invalid card number. Please try again.")
-      gameContinue()
+  if (input < 1 || input > handSize) {
+    println("Invalid card number. Please try again.")
+    gameContinue()
+  } else {
+    val card = controller.field.players(controller.field.currentPlayer).hand.cards(input - 1)
+    if (controller.field.players(controller.field.currentPlayer).valid(card) && card.canBePlayedOn(controller.field.topCard)) {
+      val playCommand = new PlayCommand(controller, card)
+      commandManager.doStep(playCommand) match {
+        case Success(_) =>
+          println("Card played successfully. Do you want to:")
+          println("1. Continue")
+          println("2. Undo last action")
+          println("3. Redo last undone action")
+          val choice = StdIn.readLine().toIntOption.getOrElse(-1)
+          choice match {
+            case 1 => gameContinue()
+            case 2 => undo(); gameContinue()
+            case 3 => redo(); gameContinue()
+            case _ =>
+              println("Invalid input. Please enter a number.")
+              gameContinue()
+          }
+        case Failure(exception) => println(s"Failed to play card: ${exception.getMessage}")
+      }
     } else {
-      val card = controller.field.players(controller.field.currentPlayer).hand.cards(input - 1)
-      if (controller.field.players(controller.field.currentPlayer).valid(card) && card.canBePlayedOn(controller.field.topCard)) {
-        UnoActionFactory.builder().setAction("play").setCard(card).build().executeAction(controller, controller.field.players(controller.field.currentPlayer))
-      } else {
-        println("Card does not fit. Do you want to:")
-        println("1. Draw a card")
-        println("2. Try again")
-        println("3. Quit")
-        val readLine = StdIn.readLine().toIntOption.getOrElse(-1)
-        readLine match {
-          case 1 => UnoActionFactory.builder().setAction("draw").build().executeAction(controller, controller.field.players(controller.field.currentPlayer))
-          case 2 => gameContinue()
-          case 3 => controller.notifyObservers(Event.Quit)
-          case _ =>
-            println("Invalid input. Please enter a number.")
-            gameContinue()
-        }
+      println("Card does not fit. Do you want to:")
+      println("1. Draw a card")
+      println("2. Try again")
+      println("3. Undo last action")
+      println("4. Redo last undone action")
+      println("5. Quit")
+      val readLine = StdIn.readLine().toIntOption.getOrElse(-1)
+      readLine match {
+        case 1 =>
+          val drawCommand = new DrawCommand(controller)
+          commandManager.doStep(drawCommand) match {
+            case Success(_) => // Command executed successfully
+            case Failure(exception) => println(s"Failed to draw card: ${exception.getMessage}")
+          }
+        case 2 => gameContinue()
+        case 3 => undo()
+        case 4 => redo()
+        case 5 => controller.notifyObservers(Event.Quit)
+        case _ =>
+          println("Invalid input. Please enter a number.")
+          gameContinue()
       }
     }
   }
+}
 
   override def update(e: Event): Unit = {
     e match {
@@ -95,13 +122,41 @@ class TUI(val controller: UnoController) extends Observer {
     currentPlayer.hand.cards.zipWithIndex.foreach { case (card, index) =>
       println(s"${index + 1}: ${card.getColorCode}${card.value}$RESET")
     }
-    println("Enter the number of the card you want to play:")
-    val cardNumber = StdIn.readLine().toIntOption
-    if (cardNumber.isEmpty) {
-      println("Invalid input. Please enter a number.")
-      gameContinue()
-    } else {
-      processInputLine(cardNumber.get, currentPlayer.hand.cards.length)
+    println("Enter the number of the card you want to play or choose an action:")
+    println("1. Play a card")
+    println("2. Undo last action")
+    println("3. Redo last undone action")
+    println("4. Quit")
+    val choice = StdIn.readLine().toIntOption.getOrElse(-1)
+    choice match {
+      case 1 =>
+        val cardNumber = StdIn.readLine().toIntOption
+        if (cardNumber.isEmpty) {
+          println("Invalid input. Please enter a number.")
+          gameContinue()
+        } else {
+          processInputLine(cardNumber.get, currentPlayer.hand.cards.length)
+        }
+      case 2 => undo()
+      case 3 => redo()
+      case 4 => controller.notifyObservers(Event.Quit)
+      case _ =>
+        println("Invalid input. Please enter a number.")
+        gameContinue()
+    }
+  }
+
+  def undo(): Unit = {
+    commandManager.undoStep match {
+      case Success(_) => println("Undo successful")
+      case Failure(exception) => println(s"Failed to undo: ${exception.getMessage}")
+    }
+  }
+
+  def redo(): Unit = {
+    commandManager.redoStep match {
+      case Success(_) => println("Redo successful")
+      case Failure(exception) => println(s"Failed to redo: ${exception.getMessage}")
     }
   }
 }
