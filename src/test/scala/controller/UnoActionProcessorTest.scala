16c35271d62c97431e5b6bd2654990ca7491ef63
mimi090804 <michellemartin644@yahoo.com>
2025-01-23 15:14:11 +0100

    test

--- src/test/scala/controller/UnoActionProcessorTest.scala
+++ src/test/scala/controller/UnoActionProcessorTest.scala
@@ -5,12 +5,13 @@ import controller.controllerComponent.IUnoController
 import controller.controllerComponent.ControllerIm.UnoController
 import controller.patterns.UnoActionBuilder.UnoActionBuilder
 import controller.patterns.UnoActionBuilder.UnoAction
+import controller.patterns.UnoActionStrategy
 import controller.patterns.*
 import model.*
 import model.cardComponent.cardIm.Card
 import model.cardComponent.cardValues.SEVEN
 import model.cardComponent.{cardColors, cardValues}
-import model.gameComponent.{IPlayer, IPlayerHand}
+import model.gameComponent.{IPlayer, IPlayerHand, IUnoField}
 import model.cardComponent.{ICard, cardColors, cardValues}
 import model.gameComponent.gameIm.{Player, PlayerHand, UnoField}
 import util.*
@@ -18,7 +19,10 @@ import org.scalatest.*
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.funsuite.AnyFunSuite
 import org.mockito.Mockito.*
+import org.mockito.ArgumentMatchers._
 import model.fileIoComponent.IFileIo
+import org.scalatest.flatspec.AnyFlatSpec
+
 
 class UnoActionProcessorTest extends AnyFunSuite with Matchers {
   def createInitialField(): UnoField = {
@@ -135,3 +139,100 @@ class UnoActionProcessorTest extends AnyFunSuite with Matchers {
     }
   }
 }
+
+/*
+class ConcreteUnoActionProcessorSpec extends AnyFlatSpec with Matchers {
+
+  "ConcreteUnoActionProcessor" should "validate and complete a 'play' action" in {
+    val mockController = mock(classOf[UnoController])
+    val mockPlayer = mock(classOf[IPlayer])
+    val mockCard = Card(cardColors.RED, cardValues.FIVE)
+    val mockField = mock(classOf[IUnoField])
+    val mockActionBuilder = mock(classOf[UnoActionBuilder])
+    val mockActionHandler = mock(classOf[UnoActionHandler])
+    val mockAction = mock(classOf[UnoAction])
+
+    when(mockActionBuilder.setAction("play")).thenReturn(mockActionBuilder)
+    when(mockActionBuilder.setCard(mockCard)).thenReturn(mockActionBuilder)
+    when(mockActionBuilder.build()).thenReturn(mockAction)
+    when(mockPlayer.id).thenReturn(1)
+    when(mockPlayer.valid(any())).thenReturn(true)
+    when(mockController.field).thenReturn(mockField)
+    when(mockField.topCard).thenReturn(mockCard)
+
+    val processor = new ConcreteUnoActionProcessor(mockActionBuilder)
+
+    println(s"Card to be played: ${mockField.topCard}")
+
+    processor.processAction(mockController, mockPlayer, "play")
+
+    verify(mockActionBuilder).setAction("play")
+    verify(mockActionBuilder).setCard(mockCard)
+    verify(mockAction).executeAction(mockController, mockPlayer)
+    verify(mockController).notifyObservers(Event.Play)
+  } // failed
+
+  it should "throw an exception if 'play' action is invalid" in {
+    val mockController = mock(classOf[UnoController])
+    val mockPlayer = mock(classOf[IPlayer])
+    val mockActionBuilder = mock(classOf[UnoActionBuilder])
+    val mockCard = mock(classOf[ICard])
+    val mockField = mock(classOf[IUnoField])
+
+    when(mockPlayer.id).thenReturn(0)
+    when(mockPlayer.valid(any())).thenReturn(false)
+    when(mockController.field).thenReturn(mockField)
+    when(mockField.topCard).thenReturn(mockCard)
+
+    val processor = new ConcreteUnoActionProcessor(mockActionBuilder)
+
+    val exception = intercept[IllegalArgumentException] {
+      processor.processAction(mockController, mockPlayer, "play")
+    }
+    exception.getMessage should be ("Card does not fit.")
+  } // passed
+
+  it should "throw an exception for an unknown action" in {
+    val mockController = mock(classOf[UnoController])
+    val mockPlayer = mock(classOf[IPlayer])
+    val mockActionBuilder = mock(classOf[UnoActionBuilder])
+    val mockCard = mock(classOf[ICard])
+    val mockField = mock(classOf[IUnoField])
+
+    when(mockPlayer.id).thenReturn(1)
+    when(mockController.field).thenReturn(mockField)
+    when(mockField.topCard).thenReturn(mockCard)
+
+    val processor = new ConcreteUnoActionProcessor(mockActionBuilder)
+
+    val exception = intercept[IllegalArgumentException] {
+      processor.processAction(mockController, mockPlayer, "unknown")
+    }
+    exception.getMessage should be ("Unknown action type")
+  } // passed
+
+  it should "validate and complete a 'draw' action" in {
+    val mockController = mock(classOf[UnoController])
+    val mockPlayer = mock(classOf[IPlayer])
+    val mockActionBuilder = mock(classOf[UnoActionBuilder])
+    val mockActionHandler = mock(classOf[UnoActionHandler])
+    val mockAction = mock(classOf[UnoAction])
+    val mockCard = mock(classOf[ICard])
+    val mockField = mock(classOf[IUnoField])
+
+    when(mockActionBuilder.setAction("draw")).thenReturn(mockActionBuilder)
+    when(mockActionBuilder.build()).thenReturn(mockAction)
+    when(mockPlayer.id).thenReturn(0)
+    when(mockController.field).thenReturn(mockField)
+    when(mockField.topCard).thenReturn(mockCard)
+
+    val processor = new ConcreteUnoActionProcessor(mockActionBuilder)
+
+    processor.processAction(mockController, mockPlayer, "draw")
+
+    verify(mockActionBuilder).setAction("draw")
+    verify(mockAction).executeAction(mockController, mockPlayer)
+    verify(mockController).notifyObservers(Event.Draw)
+  } // failed
+}
+*/
